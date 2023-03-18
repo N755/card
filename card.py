043ef50a30cd8621369c6b7624f1b97480e353ac
Anastasiia Sirenko <nastya9701@ukr.net>
2023-03-18 20:38:35 +0200

    card.py

--- card.py
+++ card.py
@@ -1,24 +1,25 @@
+from typing import List
 from faker import Faker
 fake = Faker()
 
 class Contact:
-    def __init__(self, first_name, last_name, email, phone_number):
+    def __init__(self, first_name: str, last_name: str, email: str, phone_number: str) -> None:
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.phone_number = phone_number
    
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.first_name} {self.last_name} {self.email}"
    
-    def contact(self):
+    def contact(self) -> str:
         name_for_commun = input("Будь ласка, введіть ім'я зі списку: ")
         for contact in my_contacts:
          if contact.first_name == name_for_commun:
             print (f"Зв'язуюся з {contact.first_name} {contact.last_name}, {contact.email}")
    
     @property
-    def lenght(self):
+    def lenght(self) -> int:
         return len(self.first_name) + len(self.last_name)
 
 card = Contact(first_name="David", last_name="Kyler", email="kylerd@gmail.com", phone_number = '123456789')
@@ -30,17 +31,17 @@ my_contacts = [card,card2,card3,card4,card5]
 
 
 class BaseContact(Contact):
-    def __init__(self, first_name, last_name, company, job, email, phone_number):
+    def __init__(self, first_name: str, last_name: str, company: str, job: str, email: str, phone_number: str) -> None:
         super().__init__(first_name, last_name, email, phone_number)
         self.company = company
         self.job = job
 
-    def contact(self):
+    def contact(self) -> str:
         name_for_commun = input("Будь ласка, введіть ім'я зі списку: ")
         for contact in my_contacts:
          if contact.first_name == name_for_commun:
             print (f"Я набираю {self.phone_number} і телефоную {contact.first_name} {contact.last_name}")
-#Чому коли я хочу зробити 4 пробіли біля card1,card2,card3,card4,card5 стає не активною назва класу після = ?
+
 card1 = BaseContact(first_name="David", last_name="Kyler", company="Apple", job="manager", email="kylerd@gmail.com", phone_number = '123456789')
 card2 = BaseContact(first_name="Brain", last_name="Coyle", company="Google", job="director",email="coyle1234@gmail.com", phone_number = '987654321')
 card3 = BaseContact(first_name="Helen", last_name="Ames", company="Food", job="driver", email="helenka@gmail.com", phone_number = '123789456')
@@ -52,11 +53,11 @@ for card in  my_contacts:
 card.contact() 
 
 class BusinessContact(BaseContact): 
-    def __init__(self, first_name, last_name, company, job, email, phone_number, job_phone_number ):
+    def __init__(self, first_name: str, last_name: str, company: str, job: str, email: str, phone_number: str, job_phone_number:str) -> None:
         super().__init__(first_name, last_name, company, job, email, phone_number)
         self.job_phone_number = job_phone_number
 
-    def contact(self):
+    def contact(self) -> str:
         name_for_commun = input("Будь ласка, введіть ім'я зі списку: ")
         for contact in my_contacts:
          if contact.first_name == name_for_commun:
@@ -72,7 +73,7 @@ for card in  my_contacts:
    print(f'{card}, {card.lenght}')
 card.contact()
 
-def create_contacts():
+def create_contacts() -> List:
     type_card = input('Please enter type card: base or business: ')
     quantity_card = int(input('Enter quantity of cards: '))
     if type_card == 'base':
